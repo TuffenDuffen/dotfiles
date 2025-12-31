eb75ad7b6731c654a41c0ee5e42f1f9a72e248d0
TuffeDuffen <50796186+TuffenDuffen@users.noreply.github.com>
2025-12-31 16:49:48 +0100

    battery icon changes

--- dot_config/sway/executable_status.sh
+++ dot_config/sway/executable_status.sh
@@ -4,7 +4,21 @@ function get_status
 {
 	date_formatted=$(date "+%a %F %H:%M")
 
-	battery=$(cat /sys/class/power_supply/BAT0/capacity)
+	battery_level=$(cat /sys/class/power_supply/BAT0/capacity)
+	battery_status=$(cat /sys/class/power_supply/BAT0/status)
+	
+	if [[ $battery_status =~ "Charging" ]]
+	then
+		battery_symbol="󰂄"
+	elif [[ $battery_level -lt 30 ]]
+	then
+		battery_symbol="󱊡"
+	elif [[ $battery_level -lt 60 ]]
+	then
+		battery_symbol="󱊢"
+	else
+		battery_symbol="󱊣"
+	fi
 
-	echo "${battery}%" $date_formatted
+	echo -e "${battery_symbol}${battery_level}%" $date_formatted
 }
