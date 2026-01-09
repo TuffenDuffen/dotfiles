f9bead20f507d9da537e2d1a8d66c7d17090cd0b
TuffenDuffen <50796186+TuffenDuffen@users.noreply.github.com>
2026-01-09 09:30:43 +0100

    Debian  2.0

--- dot_config/sway/executable_status.sh
+++ dot_config/sway/executable_status.sh
@@ -1,24 +1,24 @@
 #!/bin/sh
 
-function get_status 
+get_status() 
 {
 	date_formatted=$(date "+%a %F %H:%M")
 
 	battery_level=$(cat /sys/class/power_supply/BAT0/capacity)
 	battery_status=$(cat /sys/class/power_supply/BAT0/status)
 	
-	if [[ $battery_status =~ "Charging" ]]
+	if [ $battery_status =~ "Charging" ]
 	then
 		battery_symbol="󰂄"
-	elif [[ $battery_level -lt 30 ]]
+	elif [ $battery_level -lt 30 ]
 	then
 		battery_symbol="󱊡"
-	elif [[ $battery_level -lt 60 ]]
+	elif [ $battery_level -lt 60 ]
 	then
 		battery_symbol="󱊢"
 	else
 		battery_symbol="󱊣"
 	fi
 
-	echo -e "${battery_symbol}${battery_level}%" $date_formatted
+	echo "${battery_symbol}${battery_level}%" $date_formatted
 }
